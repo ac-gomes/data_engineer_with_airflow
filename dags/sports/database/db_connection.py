9d49b21ff949b68cb4994f377b19703469412f62
ac-gomes <acgoms88@gmail.com>
2023-06-12 16:54:50 -0300

    init data source

--- dags/sports/database/db_connection.py
+++ dags/sports/database/db_connection.py
@@ -5,9 +5,7 @@ from sqlalchemy import create_engine
 
 
 load_dotenv(find_dotenv())
-SOURCE_DATABASE_URL = getenv('SOURCE_DATABASE_URL')
-LANDING_DATABASE_URL = getenv('LANDING_DATABASE_URL')
+SOURCE_POSTGRES_URL = getenv('SOURCE_POSTGRES_URL')
 
-source_engine = create_engine(SOURCE_DATABASE_URL, echo=True)
 
-landing_engine = create_engine(LANDING_DATABASE_URL, echo=True)
+source_engine = create_engine(SOURCE_POSTGRES_URL, echo=True)
---
+++ dags/sports/database/init_database.py
@@ -0,0 +1,17 @@
+from sqlalchemy_utils import database_exists, create_database
+from db_connection import source_engine
+
+
+def create_source_database():
+    """ Delete and recreate tables from source """
+    try:
+        if not database_exists(source_engine.url):
+            create_database(source_engine.url)
+        print(f'is database available? {database_exists(source_engine.url)}')
+
+    except Exception as Error:
+        f'Something went wrong: {Error}'
+
+
+if __name__ == '__main__':
+    create_source_database()
